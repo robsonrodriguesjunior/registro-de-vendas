6a33d41cd92097466d2584434a4b74be049a156c
Robson Rodrigues Junior <robson.rodrigues477557@outlook.com>
2024-02-25 16:51:55 -0300

    Adicionado lombok ao Client.java

--- src/main/java/com/github/robsonrodriguesjunior/registrodevendas/domain/Client.java
+++ src/main/java/com/github/robsonrodriguesjunior/registrodevendas/domain/Client.java
@@ -1,11 +1,23 @@
 package com.github.robsonrodriguesjunior.registrodevendas.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Data;
 
 /**
  * A Client.
@@ -13,6 +25,7 @@ import java.util.Set;
 @Entity
 @Table(name = "client")
 @SuppressWarnings("common-java:DuplicatedBlocks")
+@Data
 public class Client implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -36,104 +49,4 @@ public class Client implements Serializable {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "client")
     @JsonIgnoreProperties(value = { "client", "seller", "products" }, allowSetters = true)
     private Set<Sale> buys = new HashSet<>();
-
-    // jhipster-needle-entity-add-field - JHipster will add fields here
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public Client id(Long id) {
-        this.setId(id);
-        return this;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return this.code;
-    }
-
-    public Client code(String code) {
-        this.setCode(code);
-        return this;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public Person getPerson() {
-        return this.person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
-
-    public Client person(Person person) {
-        this.setPerson(person);
-        return this;
-    }
-
-    public Set<Sale> getBuys() {
-        return this.buys;
-    }
-
-    public void setBuys(Set<Sale> sales) {
-        if (this.buys != null) {
-            this.buys.forEach(i -> i.setClient(null));
-        }
-        if (sales != null) {
-            sales.forEach(i -> i.setClient(this));
-        }
-        this.buys = sales;
-    }
-
-    public Client buys(Set<Sale> sales) {
-        this.setBuys(sales);
-        return this;
-    }
-
-    public Client addBuys(Sale sale) {
-        this.buys.add(sale);
-        sale.setClient(this);
-        return this;
-    }
-
-    public Client removeBuys(Sale sale) {
-        this.buys.remove(sale);
-        sale.setClient(null);
-        return this;
-    }
-
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Client)) {
-            return false;
-        }
-        return getId() != null && getId().equals(((Client) o).getId());
-    }
-
-    @Override
-    public int hashCode() {
-        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
-        return getClass().hashCode();
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "Client{" +
-            "id=" + getId() +
-            ", code='" + getCode() + "'" +
-            "}";
-    }
 }
