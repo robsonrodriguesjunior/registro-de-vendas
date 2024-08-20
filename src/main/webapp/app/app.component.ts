9e7966f01a98663a69198d523107e5a07ad1c09e
Robson Rodrigues Junior <robson.rodrigues477557@outlook.com>
2024-08-19 22:28:00 -0300

    Adicionado rota de login como padrão

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import locale from '@angular/common/locales/en';
-import dayjs from 'dayjs/esm';
+import { Component } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import dayjs from 'dayjs/esm';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
+import { Router } from '@angular/router';
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
 import { fontAwesomeIcons } from './config/font-awesome-icons';
+import { AccountService } from './core/auth/account.service';
 import MainComponent from './layouts/main/main.component';
 
 @Component({
@@ -20,10 +22,19 @@ import MainComponent from './layouts/main/main.component';
   ],
 })
 export default class AppComponent {
-  constructor(applicationConfigService: ApplicationConfigService, iconLibrary: FaIconLibrary, dpConfig: NgbDatepickerConfig) {
+  constructor(
+    applicationConfigService: ApplicationConfigService,
+    iconLibrary: FaIconLibrary,
+    dpConfig: NgbDatepickerConfig,
+    private accountService: AccountService,
+    private router: Router,
+  ) {
     applicationConfigService.setEndpointPrefix(SERVER_API_URL);
     registerLocaleData(locale);
     iconLibrary.addIcons(...fontAwesomeIcons);
     dpConfig.minDate = { year: dayjs().subtract(100, 'year').year(), month: 1, day: 1 };
+    if (accountService.isAuthenticated()) {
+      this.router.navigate(['']);
+    }
   }
 }
